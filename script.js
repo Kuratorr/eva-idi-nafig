9d1ba1847cdcfc56408319205f7db03e954bf361
Kuratorr <132139583+Kuratorr@users.noreply.github.com>
2024-01-30 17:55:23 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function run() {
+    let button = document.querySelector('.btn')
+    button.value = 'иди нахуй'
+}
+
+let button = document.querySelector('.btn');
+button.addEventListener('click', run);
